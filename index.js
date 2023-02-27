5d9e5cd2d8668fc279d079fe38d8cda81c56c6db
arioliveirari <91267797+arioliveirari@users.noreply.github.com>
2023-02-27 18:43:57 -0300

    changes

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 
 
+function fetchPokemon(){
+    fetch('https://pokeapi.co/api/v2/pokemon')
+     .then(response => response.json())
+     .then(function(allpokemon){
+     allpokemon.results.forEach(function(pokemon){
+       fetchPokemonData(pokemon); 
+     })
+    })
+   }
 
-   async function getPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/'+Attr[0]+'/';
-    try {
-        let res = await fetch(url);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
+
+function fetchPokemonData(pokemon){
+    let url = pokemon.url
+      fetch(url)
+      .then(response => response.json())
+      .then(function(pokeData){
+      console.log(pokeData.id)
+      })
     }
-}
 
+fetchPokemon()
+//console.log(test)
 
-let test = getPokemon(1)
-console.log(test.id)
+function renderPokemon(pokeData){
+        let allPokemonContainer = document.getElementById('pokedex');
+        let pokeContainer = document.createElement("div") //div will be used to hold the data/details for indiviual pokemon.{}
+        let pokeName = document.createElement('h4')       // pokemon name
+        pokeName.innerText = pokeData.name
+        let pokeNumber = document.createElement('p')      //pokemon id                      
+        pokeNumber.innerText = `#${pokeData.id}`
+        pokeContainer.append(pokeName, pokeNumber);       //appending all details to the pokeContainer div
+        allPokemonContainer.appendChild(pokeContainer);   //appending that pokeContainer div to the main div which will                                                             hold all the pokemon cards
+        }
+
+renderPokemon()
+
+function createTypes(types, ul){
+            types.forEach(function(type){
+            let typeLi = document.createElement('li');
+            typeLi.innerText = type['type']['name'];
+            ul.append(typeLi)
+            })
+          }
+
+function createPokeImage(pokeID, containerDiv){
+            let pokeImage = document.createElement('img')
+            pokeImage.srcset =    `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
+            containerDiv.append(pokeImage);
+          }
+
